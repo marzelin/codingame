98827db43470870766a5d6734a10ecfb8e0cd9cd
marzelin <marzelin@gmail.com>
2017-02-26 22:47:49 +0100

    initial commit

---
+++ codingame.d.ts
@@ -0,0 +1,20 @@
+/**
+ * reads a line from an input
+ * 
+ * @returns {string} 
+ */
+declare function readline(): string
+
+/**
+ * prints given arg to the output
+ * 
+ * @param {(string | number)} output 
+ */
+declare function print(output: string | number): void
+
+/**
+ * prints debugging messages
+ * 
+ * @param {*} output 
+ */
+declare function printErr(output: any): void
