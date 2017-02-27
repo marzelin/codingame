2589055a8ed8ae17a474e8f8574e18b2f9a1a065
marzelin <marzelin@gmail.com>
2017-02-27 02:19:49 +0100

    add bomb handling, fix and rewrite

--- sync.ts
+++ sync.ts
@@ -1,7 +1,6 @@
 const run = () => {
   /** distances between factories */
   const distances = processInitialInput()
-  printErr(`distances ` + Object.keys(distances))
   /** bombs available to detonate */
   let availableBombs = 2
   
@@ -15,6 +14,7 @@ const run = () => {
       orders: []
     }
 
+    state = maybeBomb(state)
     state = planOrders(state)
     print(state.orders.join('; ') || 'WAIT')
     availableBombs = state.availableBombs
@@ -113,10 +113,48 @@ const processBomb = (args: string[], factories: Ifactories) => {
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////// PLAN ORDERS
 
+const maybeBomb = (state: Istate) => {
+  let factories = state.factories
+  let target = Object.keys(factories)
+  .map( (id) => factories[Number(id)])
+  .filter(enemyOnly)
+  .filter(productionGte2only)
+  .filter(noCaptureInProgress)
+  .filter(noBombTarget)
+  .filter(noFrozen)
+  .sort(byProduction)[0]
+  if (target) {
+    /** the factory from which the bomb will be launched */
+    let bombLaunchFactory = getFactories(state)
+    .filter(mineOnly)
+    .sort(byDistance(state.distances, target.id))[0]
+    if (bombLaunchFactory) {
+      const newAvailableBombs = state.availableBombs - 1
+      const newOrder = `BOMB ${bombLaunchFactory.id} ${target.id}`
+      const newOrders = state.orders.slice().concat(newOrder)
+      target = Object.assign({}, target, {
+        isBombTarget: true
+      })
+      const newFactories = Object.assign({}, factories, {
+        [target.id]: target
+      })
+      const newState: Istate = Object.assign({}, state, {
+        factories: newFactories,
+        orders: newOrders,
+        availableBombs: newAvailableBombs 
+      })
+      return newState
+    }
+  }
+  return state
+}
+
 const planOrders = (state: Istate) => {
   let factories = state.factories
   return Object.keys(factories)
   .map( (id) => factories[Number(id)])
+  .filter(productiveOnly)
+  .filter(noBombTarget)
   .sort(byProduction)
   .map( (factory) => Number(factory.id))
   .reduce(handle, state)
@@ -125,24 +163,22 @@ const planOrders = (state: Istate) => {
 const handle = (state: Istate, targetId: number) => {
   let factories = state.factories
   const targetFactory = factories[targetId]
-  if (targetFactory.owner !== OwnBy.me) {
+  const attacker = Object.keys(targetFactory.incomingTroops)
+  .map(Number) .sort()
+  .map( (daysToArrival) => targetFactory.incomingTroops[daysToArrival])[0]
+  if (targetFactory.owner !== OwnBy.me
+      // if there is no incoming troops
+      // or they are not mine
+      && !(attacker && attacker.size > 0)) {
     const myFactories = Object.keys(factories)
     .map( (id) => factories[Number(id)] )
     .filter( ({owner}) => owner === OwnBy.me)
     .sort(byDistance(state.distances, targetId))
     for (let myFactory of myFactories) {
-      printErr('myFactory ' + myFactory.id)
-      printErr('targetFactory ' + targetId)
-      printErr(`${myFactory.id}:${targetId}`)
       const distance = state.distances[`${myFactory.id}:${targetId}`]
-      printErr('distance keys ' + Object.keys(state.distances))
       const myCyborgs = myFactory.availableCybors 
-      const enemyCyborgs = targetFactory.availableCybors + targetFactory.production * distance 
-      printErr('targetFactory.availableCybors ' + targetFactory.availableCybors)
-      printErr('targetFactory.production ' + targetFactory.production)
-      printErr('distance ' + distance)
-      printErr('myCyborgs ' + myCyborgs)
-      printErr('enemyCyborgs ' + enemyCyborgs)
+      let enemyCyborgs = targetFactory.availableCybors
+      if (targetFactory.owner === OwnBy.enemy) { enemyCyborgs += targetFactory.production * (distance + 1) }
       if (myCyborgs > enemyCyborgs) {
         myFactory = Object.assign({}, myFactory, {
           availableCybors: myFactory.availableCybors - enemyCyborgs - 1
@@ -176,6 +212,41 @@ const byDistance =
   (distances: Idinstance, sourceId: number) =>
   (factory1: Ifactory, factory2: Ifactory) => 
   distances[`${sourceId}:${factory1.id}`] - distances[`${sourceId}:${factory2.id}`]
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////// FILTERS
+
+/** 
+ * predicate for filter that saves
+ * only factories with production
+ * greater than 0
+ */
+const productiveOnly = (factory: Ifactory) => factory.production > 0
+
+const enemyOnly = (factory: Ifactory) => factory.owner === OwnBy.enemy
+
+const mineOnly = (factory: Ifactory) => factory.owner === OwnBy.me
+
+const productionGte2only = (factory: Ifactory) => factory.production >= 2
+
+const noCaptureInProgress = (factory: Ifactory) => {
+  const incomingTroopsCount = Object.keys(factory.incomingTroops)
+  .map( (key) => factory.incomingTroops[Number(key)] )
+  .reduce( (sum, troop) => sum + troop.size, 0)
+  return incomingTroopsCount <= 0
+}
+
+const noBombTarget = (factory: Ifactory) => !factory.isBombTarget
+const noFrozen = (factory: Ifactory) => !factory.frozenDays
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////// UTILITIES
+
+const getFactories = (state: Istate) => Object.keys(state.factories)
+  .map( (id) => state.factories[Number(id)])
+
+const getTroops = (factory: Ifactory) => Object.keys(factory.incomingTroops)
+  .map( (arrivalDay) => factory.incomingTroops[Number(arrivalDay)])
 
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////// INTERFACES
